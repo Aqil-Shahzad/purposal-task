11df486ea6a4712bf707e39452b950334406f97c
Aqil-Shahzad <122356773+Aqil-Shahzad@users.noreply.github.com>
2023-02-24 17:26:11 +0500

    completed Except Logout

--- src/component/pages-partials/project/index.js
+++ src/component/pages-partials/project/index.js
@@ -1,20 +1,29 @@
 import "../../../app/globals.css";
 import Image from "next/image";
 import { app, database } from "../../firebase";
-import { collection, addDoc, getDocs } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
 import { useState, useEffect } from "react";
 import { data } from "autoprefixer";
 
 export default function Project() {
   const databaseRef = collection(database, "CRUD data");
+  const [ID, setID] = useState(null);
   const [name, setName] = useState("");
   const [age, setAge] = useState(null);
   const [fireData, setFireData] = useState([]);
+  const [isUpdate, setIsUpdate] = useState(false);
 
   // // useEffect(() => {
   // //   let token = sessionStorage.getItem("Token");
   // //   if (token) {
-     
+
   // //   }
   //   // if (!token) {
   //   //   router.push('/register')
@@ -44,6 +53,45 @@ export default function Project() {
     });
   };
 
+  const getID = (id, name, age) => {
+    setID(id)
+    setName(name)
+    setAge(age)
+    setIsUpdate(true)
+  }
+
+  const updateFields = () => {
+    let fieldToEdit = doc(database, 'CRUD data', ID);
+    updateDoc(fieldToEdit, {
+      name: name,
+      age: Number(age)
+    })
+    .then(() => {
+      alert('Data Updated')
+      getData()
+      setName('')
+      setAge(null)
+      setIsUpdate(false)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+
+  const deleteDocument = (id) => {
+    let fieldToEdit = doc(database, "CRUD data", id);
+    console.log(fieldToEdit);
+    deleteDoc(fieldToEdit)
+      .then(() => {
+        alert("Data Deleted");
+        getData();
+      })
+      .catch((err) => {
+        alert("Cannot Delete that field..");
+      });
+  };
+
   return (
     <>
       <div>
@@ -69,7 +117,7 @@ export default function Project() {
             </div>
           </nav>
         </div>
-        <div className="flex flex-col justify-center items-center  py-36 space-y-8">
+        <div className="flex flex-col justify-center items-center  py-8 space-y-8">
           <h1 className="text-4xl font-bold">Home Page</h1>
           <input
             onChange={(event) => setName(event.target.value)}
@@ -83,16 +131,30 @@ export default function Project() {
             value={age}
             type="number"
             placeholder="Enter Age"
-            className="p-3 rounded-lg lg:w-80 md:w-72 w-48"
+            className="p-3  rounded-lg lg:w-80 md:w-72 w-48"
           ></input>
+         {isUpdate ? (
           <button
-            onClick={addData}
-            className="p-3 rounded-lg lg:w-80 md:w-72 w-48 background-color text-color-white"
+          className="p-3 rounded-lg lg:w-80 md:w-72 w-48 background-color text-color-white"
+            onClick={updateFields}
           >
-            Add Data
+            UPDATE
           </button>
+        ) : (
           <button
-            onClick={ getData}
+          className="p-3 rounded-lg lg:w-80 md:w-72 w-48 background-color text-color-white"
+            onClick={addData}
+          >
+            ADD
+          </button>
+        )}
+
+
+
+
+
+          <button
+            onClick={getData}
             className="p-3 rounded-lg lg:w-80 md:w-72 w-48 background-color text-color-white"
           >
             Show Data
@@ -101,17 +163,29 @@ export default function Project() {
             {fireData.map((data) => {
               return (
                 <>
-                <div className="background-color p-5 w-80 text-color-white ">
-                <h3 >Name: {data.name}</h3>
-                  <p>Age: {data.age}</p>
-                </div>
-                 
+                  <div className="background-color-gray">
+                    <div className="background-color rounded-lg p-5 w-80 text-color-white my-3 ">
+                      <h3>Name: {data.name}</h3>
+                      <p>Age: {data.age}</p>
+                      <button
+                        className="background-color-gray p-3 my-2 rounded-lg text-color-black "
+                        onClick={() => getID(data.id, data.name, data.age)}
+                      >
+                        Update
+                      </button>
+                      <button
+                        className="background-color-gray p-3 my-2 rounded-lg text-color-black ml-2"
+                        onClick={() => deleteDocument(data.id)}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </div>
                 </>
               );
             })}
           </div>
         </div>
-       
       </div>
     </>
   );
